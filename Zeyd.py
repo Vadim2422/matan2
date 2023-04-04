f1f3d59eea91c28dade2556c24f1d9271c8206dc
vadim2422 <breev.vadim@yandex.ru>
2023-04-05 00:37:14 +0300

    WIP: refactoring methods

--- Zeyd.py
+++ Zeyd.py
@@ -1,9 +1,7 @@
 import numpy as np
-from math import *
-import cmath as cm
 
 
-def Recombinate(A,B):
+def Recombinate(A, B):
     n = A.shape[0]
     A1 = np.zeros(A.shape)
     B1 = np.zeros(B.shape)
@@ -15,29 +13,29 @@ def Recombinate(A,B):
 
 
 def result(A, b, eps):
-    A,b = Recombinate(A,b)
+    A, b = Recombinate(A, b)
     n = A.shape[0]
     g = np.zeros(n)
     H = np.zeros(A.shape)
- 
+
     for i in range(n):
         g[i] = b[i] / A[i][i]
- 
+
     for i in range(n):
         for j in range(n):
             if i != j:
                 H[i][j] = -A[i][j] / A[i][i]
- 
+
     X = np.zeros(n)
     Xprev = np.zeros(n)
- 
-    delta = eps+1
+
+    delta = eps + 1
     itter = 0
     while delta > eps:
-        itter+=1
+        itter += 1
         for i in range(n):
             X[i] = g[i] + H[i].dot(X)
-         
+
         delta = max([abs(min(X - Xprev)), abs(max(X - Xprev))])
         Xprev = np.copy(X)
-    return X,itter
+    return X, itter
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import numpy as np
-# from v_matrix import *
-from l_matrix import *
+from matrix import *
 from math import *
-import cmath as cm
 
 import Gaus
 import LU
@@ -12,64 +9,62 @@ import Iter
 import Zeyd
 
 
-
-def Newyazka(A,B,X):
-    Bruh = np.array([sum(A[i]*X) for i in range(A.shape[0])])
-    result = sqrt(sum((B-Bruh)**2))
+def Newyazka(A, B, X):
+    Bruh = np.array([sum(A[i] * X) for i in range(A.shape[0])])
+    result = sqrt(sum((B - Bruh) ** 2))
     return result
 
-print("Классический метод Гаусса")
-X = Gaus.result(np.copy(A1), np.copy(B1))
-print("Гаус1 простой X:", X, "   ||B-B\'|| = ", Newyazka(A1, B1, X))
-X = Gaus.result(np.copy(A2), np.copy(B2))
-print("Гаус2 простой X:", X, "   ||B-B\'|| = ", Newyazka(A2, B2, X))
-X = Gaus.result(np.copy(A3), np.copy(B3))
-print("Гаус3 простой X:", X, "   ||B-B\'|| = ", Newyazka(A3, B3, X))
-print()
 
-print("Метод Гаусса с частичным выбором главного элемента")
-X = Gaus.result_nofull(np.copy(A1), np.copy(B1))
-print("Гаус1 частичный X:", X, "   ||B-B\'|| = ", Newyazka(A1, B1, X))
-X = Gaus.result_nofull(np.copy(A2), np.copy(B2))
-print("Гаус2 частичный X:", X, "   ||B-B\'|| = ", Newyazka(A2, B2, X))
-X = Gaus.result_nofull(np.copy(A3), np.copy(B3))
-print("Гаус3 частичный X:", X, "   ||B-B\'|| = ", Newyazka(A3, B3, X))
-print()
+# print("Классический метод Гаусса")
+# X = Gaus.result(np.copy(A1), np.copy(B1))
+# print("Гаус1 простой X:", X, "   ||B-B\'|| = ", Newyazka(A1, B1, X))
+# X = Gaus.result(np.copy(A2), np.copy(B2))
+# print("Гаус2 простой X:", X, "   ||B-B\'|| = ", Newyazka(A2, B2, X))
+# X = Gaus.result(np.copy(A3), np.copy(B3))
+# print("Гаус3 простой X:", X, "   ||B-B\'|| = ", Newyazka(A3, B3, X))
+# print()
+#
+# print("Метод Гаусса с частичным выбором главного элемента")
+# X = Gaus.result_nofull(np.copy(A1), np.copy(B1))
+# print("Гаус1 частичный X:", X, "   ||B-B\'|| = ", Newyazka(A1, B1, X))
+# X = Gaus.result_nofull(np.copy(A2), np.copy(B2))
+# print("Гаус2 частичный X:", X, "   ||B-B\'|| = ", Newyazka(A2, B2, X))
+# X = Gaus.result_nofull(np.copy(A3), np.copy(B3))
+# print("Гаус3 частичный X:", X, "   ||B-B\'|| = ", Newyazka(A3, B3, X))
+# print()
 
 print("Метод Гаусса с полным выбором главного элемента")
 X = Gaus.result_full(np.copy(A1), np.copy(B1))
-print("Гаус1 полный X:", X, "   ||B-B\'|| = ", Newyazka(A1, B1, X))
+print("Матрица 1\nX:", X, "   Невязка = ", Newyazka(A1, B1, X))
 X = Gaus.result_full(np.copy(A2), np.copy(B2))
-print("Гаус2 полный X:", X, "   ||B-B\'|| = ", Newyazka(A2, B2, X))
+print("Матрица 2\nX:", X, "   Невязка = ", Newyazka(A2, B2, X))
 X = Gaus.result_full(np.copy(A3), np.copy(B3))
-print("Гаус3 полный X:", X, "   ||B-B\'|| = ", Newyazka(A3, B3, X))
+print("Матрица 3\nX:", X, "   Невязка = ", Newyazka(A3, B3, X))
 print()
 
-
-
-L,U = LU.decomp(A2)
-X = LU.result(L,U,B2)
-print("LU X:", X, "   ||B-B\'|| = ", Newyazka(A2, B2, X))
+L, U = LU.decomp(A2)
+X = LU.result(L, U, B2)
+print("LU - разложение\nX:", X, "   Невязка = ", Newyazka(A2, B2, X))
 print()
 
 L = LLT.decomp(A2)
-X = LLT.result(L,B2)
-print("LLT X:", X, "   ||B-B\'|| = ", Newyazka(A2, B2, X))
+X = LLT.result(L, B2)
+print("LLT - разложение\nX:", X, "   Невязка = ", Newyazka(A2, B2, X))
 print()
 
 X = Progon.result(A3, B3)
-print("Прогонка X:", X, "   ||B-B\'|| = ", Newyazka(A3, B3, X))
+print("Прогонка\nX:", X, "   Невязка = ", Newyazka(A3, B3, X))
 print()
 
 A = np.copy(A1)
 B = np.copy(B1)
-X,itter = Iter.result(A, B, 0.001)
-print("Итерация X:", X, "   ||B-B\'|| = ", Newyazka(A, B, X))
+X, itter = Iter.result(A, B, 0.001)
+print("Простая итерация\nX:", X, "   Невязка = ", Newyazka(A, B, X))
 print("колличество иттераций: ", itter)
 print()
 
 A = np.copy(A1)
 B = np.copy(B1)
-X,itter = Zeyd.result(A, B, 0.001)
-print("Зейдель  X:", X, "   ||B-B\'|| = ", Newyazka(A, B, X))
+X, itter = Zeyd.result(A, B, 0.001)
+print("Зейдель\nX:", X, "   Невязка = ", Newyazka(A, B, X))
 print("колличество иттераций: ", itter)
